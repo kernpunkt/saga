9e80db200d964ac36acb646f70c6f6143107714c
Jörn Meyer <joern.meyer@kernpunkt.de>
2025-01-28 16:10:56 +0100

    feat: tests added

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+};
